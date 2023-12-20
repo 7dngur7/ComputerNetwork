08c50c0f3e0332325ed2756a8204289b6f304e53
woohyuk <7dngur7@naver.com>
2023-12-20 14:44:43 +0900

    socket programing - chatting

--- src/TestClient.java
+++ src/TestClient.java
@@ -1,33 +1,46 @@
 import java.io.*;
 import java.net.*;
 
-class TestClient {
+public class TestClient {
+
     public static void main(String[] args) throws Exception {
+        // 클라이언트에서 서버로 보낼 메시지를 저장할 변수
         String sentence;
+        // 서버로부터 받은 메시지를 저장할 변수
         String fromServer;
 
-        // 사용자로부터 입력 받을 BufferedReader 생성
+        // 사용자로부터 입력을 받기 위한 BufferedReader 생성
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        // 서버에 연결할 소켓 생성
+        // localhost의 6789 포트에 연결할 클라이언트 소켓 생성
         Socket clientSocket = new Socket("localhost", 6789);
 
-        // 서버에 데이터를 보낼 출력 스트림 생성
+        // 서버로 데이터를 보낼 출력 스트림 생성
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
         // 서버로부터 데이터를 받을 입력 스트림 생성
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        // 사용자로부터 메시지 입력을 받고 서버로 전송
-        System.out.println("서버에 보낼 메시지를 입력하세요 ");
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + "\n");
+        while (true) {
+            // 사용자에게 메시지 입력 요청
+            System.out.print("보내기 > ");
+            // 사용자가 입력한 메시지를 읽기
+            sentence = inFromUser.readLine();
+            // 서버로 메시지를 보냄
+            outToServer.writeBytes(sentence + "\n");
 
-        // 서버로부터 받은 응답을 출력
-        fromServer = inFromServer.readLine();
-        System.out.println("서버로부터 받은 메시지: " + fromServer);
+            // 사용자가 입력한 메시지가 "bye"일 경우 연결 종료
+            if (sentence.equals("bye")) {
+                System.out.println("연결이 종료되었습니다.");
+                // 클라이언트 소켓을 닫고 루프를 종료함
+                clientSocket.close();
+                break;
+            }
 
-        // 연결 종료
-        clientSocket.close();
+            // 서버에서 받은 메시지를 읽음
+            fromServer = inFromServer.readLine();
+            // 서버에서 받은 메시지를 출력함
+            System.out.println("서버 > " + fromServer);
+        }
     }
 }
